ec206febbc2dc40e460ca18f9853e028faaba491
mkcre <99949382+mkcre@users.noreply.github.com>
2025-04-13 15:50:32 +0000

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -10,7 +10,7 @@ export default defineUserConfig({
 
   theme: defaultTheme({
     base: '/carbon_book/',
-    dest: 'dist',
+    dest: 'public',
     logo: 'https://vuejs.press/images/hero.png',
     navbar: [
       {
